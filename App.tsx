2b9a86aad409e880213c68435a7b2ba5b5988d16
denkaforss <dennis.forss@gmail.com>
2022-06-13 11:57:02 +0200

    Created components and views

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import LoginForm from "./src/components/LoginForm";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Detta är en text.</Text>
+      <LoginForm />
       <StatusBar style="auto" />
     </View>
   );
