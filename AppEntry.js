e329cd3c135fdbe2e1a9dca02ac9ea4a8e4a7594
DouglasAlexa <92862853+DouglasAlexa@users.noreply.github.com>
2022-06-28 16:02:31 +0200

    Minor updates

--- AppEntry.js
+++ AppEntry.js
@@ -34,7 +34,7 @@ export default function AppEntry() {
       setStatus("success");
     } catch (error) {
       setStatus("error");
-      console.log(`Keychain Error: ${error.message}`);
+      // console.log(`Keychain Error: ${error.message}`);
       authContext.setAuthState({
         accessToken: null,
         refreshToken: null,
