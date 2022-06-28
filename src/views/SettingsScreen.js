b300617cbfb3ccca6b00a59217e255fd5fdf0e9f
denkaforss <dennis.forss@gmail.com>
2022-06-28 14:48:50 +0200

    Created SettingsScreen view and renamed the bottom tabs

---
+++ src/views/SettingsScreen.js
@@ -0,0 +1,48 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>This is the settings screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 25,
+    borderColor: "#777",
+    padding: 15,
+    width: 250,
+    backgroundColor: "#fff",
+  },
+  btns: {
+    width: "50%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    backgroundColor: "#2D232E",
+  },
+  btnText: {
+    color: "#fff",
+  },
+});
+
+export default SettingsScreen;
