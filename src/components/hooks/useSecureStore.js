40559162e943697c212936adfbf70e943f82cce6
DouglasAlexa <92862853+DouglasAlexa@users.noreply.github.com>
2022-06-28 15:24:38 +0200

    Update useSecureStore.js

--- src/components/hooks/useSecureStore.js
+++ src/components/hooks/useSecureStore.js
@@ -7,11 +7,6 @@ export async function save(key, value) {
 
 export async function getValueFor(key) {
   let result = await SecureStore.getItemAsync(key);
-  if (result) {
-    return result;
-  } else {
-    Alert.alert(`No Value Found With That Key: ${key}`);
-  }
   return result;
 }
 
