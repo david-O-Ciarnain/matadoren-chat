4d44dbb6b72effd7ccf08df0509b5b61c7370692
denkaforss <dennis.forss@gmail.com>
2022-06-29 14:33:45 +0200

    updateuserform WIP

--- src/components/UpdateUserForm.js
+++ src/components/UpdateUserForm.js
@@ -11,47 +11,36 @@ import { BASE_URL } from "@env";
 import { AuthContext } from "../context/AuthContext";
 import jwtDecode from "jwt-decode";
 import { User } from "../models/User";
+import axios from "axios";
 
 const UpdateUserForm = () => {
+  const [username, setUsername] = useState("");
+  const authContext = useContext(AuthContext);
 
-    const [username, setUsername] = useState("");
-    const authContext = useContext(AuthContext);
-  
-    const [user, setUser] = useState(User);
-  
-    useEffect(() => {
-      const token = authContext.authState.accessToken;
-      const decodedToken = jwtDecode(token);
-      setUser(decodedToken.sub.toString());
-    }, []);
-  
-    //   const getCurrentUsername = () => {
-    //     fetch(BASE_URL + "/get")
-    //       .then((res) => res.stringify)
-    //       .then((res) => {
-    //         setCurrentUsername(res);
-    //         console.log("Username retrieved", res);
-    //       })
-    //       .catch((err) => console.log("Something went wrong ", err));
-    //   };
-  
-    const changeUsername = () => {
-      fetch(`${BASE_URL}/update/${user.username}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: {JSON.stringif},
-      })
-        .then((data) => JSON.parse(data))
-        .then((data) => {
-          Alert.alert(`Success! Username changed to: ${data.message}`);
-          // authContext.logout();
-        })
-        .catch((err) => {
-          console.log("Error: ", err);
-        });
-    };
+  const [user, setUser] = useState(User);
+
+  useEffect(() => {
+    const token = authContext.authState.accessToken;
+    const decodedToken = jwtDecode(token);
+    setUser(decodedToken.sub.toString());
+  }, []);
+
+  //   const getCurrentUsername = () => {
+  //     fetch(BASE_URL + "/get")
+  //       .then((res) => res.stringify)
+  //       .then((res) => {
+  //         setCurrentUsername(res);
+  //         console.log("Username retrieved", res);
+  //       })
+  //       .catch((err) => console.log("Something went wrong ", err));
+  //   };
+
+  const changeUsername = () => {
+    const body = { username: { username } };
+    const headers = {};
+    axios.put(`${BASE_URL}/update/${user.username}`, body, headers);
+  };
+
   return (
     <View>
       <Text>Current username: {user}</Text>
@@ -63,14 +52,42 @@ const UpdateUserForm = () => {
         placeholder="New username"
         textContentType="username"
       />
-      <TouchableOpacity
-        style={styles.btns}
-        onPress={() => changeUsername(user, username)}
-      >
+      <TouchableOpacity style={styles.btns} onPress={() => changeUsername()}>
         <Text style={styles.btnText}>OK</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 25,
+    borderColor: "#777",
+    padding: 15,
+    width: 250,
+    backgroundColor: "#fff",
+  },
+  btns: {
+    width: "50%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    backgroundColor: "#2D232E",
+  },
+  btnText: {
+    color: "#fff",
+  },
+});
 
 export default UpdateUserForm;
--- src/views/SettingsScreen.js
+++ src/views/SettingsScreen.js
@@ -22,35 +22,4 @@ const SettingsScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    height: 50,
-    margin: 12,
-    borderWidth: 1,
-    borderRadius: 25,
-    borderColor: "#777",
-    padding: 15,
-    width: 250,
-    backgroundColor: "#fff",
-  },
-  btns: {
-    width: "50%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-    backgroundColor: "#2D232E",
-  },
-  btnText: {
-    color: "#fff",
-  },
-});
-
 export default SettingsScreen;
